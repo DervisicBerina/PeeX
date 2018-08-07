9e95b0a17c250db573d2698bf45de7b4254c28a2
Berina Dervisic <dervisicberina@hotmail.com>
2018-08-07 11:47:35 +0200

    template updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/material'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/hello', (req, res) => res.send('Hello World!'));
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
