4c65fb67adee92f3a7890b84464d4523f0aa088c
Berina Dervisic <dervisicberina@hotmail.com>
2018-08-08 20:26:33 +0200

    Added heroku required files

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 5000
 
 app.use(express.static(__dirname + '/material'));
 
-app.get('/hello', (req, res) => res.send('Hello World!'));
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.get('/', (req, res) => res.sendStatus(200));
+app.listen(port,function(){
+  console.log('Node app is running on port', port);
+});
