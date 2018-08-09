1b3af470b593ab87e65cd567f21899c8470ef4c2
Berina Dervisic <dervisicberina@hotmail.com>
2018-08-09 13:39:02 +0200

    mongojs and body-parser installed

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('localhost:27017/peex',['expenses']);
+ var body_parser = require('body-parser');
+ app.use(body_parser.json());
+
 var port = process.env.PORT || 5000
 
 app.use(express.static(__dirname + '/material'));
+
+app.get('/expenses', function(req,res){
+  db.expenses.find(function(err,docs){
+    res.json(docs)
+})
+});
+
 
 app.get('/', (req, res) => res.sendStatus(200));
 app.listen(port,function(){
