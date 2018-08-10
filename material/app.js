46d9317443fa8b20e3b6e042dd10fd9d6074603d
Berina Dervisic <dervisicberina@hotmail.com>
2018-08-10 11:09:16 +0200

    code cleanup

--- material/app.js
+++ material/app.js
@@ -48,9 +48,3 @@ app.controller('dashboardCtrl',function(){
 // </script>
     md.initDashboardPageCharts();
 });
-app.controller('expensesCtrl',function($scope,$http){
-    $scope.myExpenses = [];
-   $http.get('/expenses').then(function(response){
-       $scope.myExpenses = response.data;
-   }) 
-});
---
+++ material/controllers/expensesCtrl.js
@@ -0,0 +1,6 @@
+app.controller('expensesCtrl',function($scope,$http){
+    $scope.myExpenses = [];
+   $http.get('/expenses').then(function(response){
+       $scope.myExpenses = response.data;
+   }) 
+});
