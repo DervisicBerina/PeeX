8a345b55ef0f5676c7d6ea63761304b7f8a7194a
Berina Dervisic <dervisicberina@hotmail.com>
2018-08-12 12:33:10 +0200

    Removing duplicates from category type list

--- material/controllers/recurringExpensesCtrl.js
+++ material/controllers/recurringExpensesCtrl.js
@@ -1,9 +1,45 @@
 app.controller('recurringExpensesCtrl', function($scope,$http){
     $scope.categoryList = [];
     $http.get('/category').then(function(response){
-        debugger
         $scope.categoryList = response.data;
         console.log(response.data);
     }) 
-   
 })
+app.filter('unique', function () {
+
+    return function (items, filterOn) {
+
+        if (filterOn === false) {
+            return items;
+        }
+
+        if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+            var hashCheck = {}, newItems = [];
+
+            var extractValueToCompare = function (item) {
+                if (angular.isObject(item) && angular.isString(filterOn)) {
+                    return item[filterOn];
+                } else {
+                    return item;
+                }
+            };
+
+            angular.forEach(items, function (item) {
+                var valueToCheck, isDuplicate = false;
+
+                for (var i = 0; i < newItems.length; i++) {
+                    if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate) {
+                    newItems.push(item);
+                }
+
+            });
+            items = newItems;
+        }
+        return items;
+    };
+});
