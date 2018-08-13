ae9d4f33bf06e1fcc195f8260c45311435fc4756
Berina Dervisic <dervisicberina@hotmail.com>
2018-08-13 21:57:05 +0200

    angular modal added

--- material/app.js
+++ material/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("PeeXApp", ['ui.router','toastr']);
+var app = angular.module("PeeXApp", ['ui.router','toastr', 'pathgather.popeye']);
 app.config(function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/login');
 
--- material/controllers/recurringExpensesCtrl.js
+++ material/controllers/recurringExpensesCtrl.js
@@ -1,6 +1,7 @@
-app.controller('recurringExpensesCtrl', function($scope,$http,toastr){
+app.controller('recurringExpensesCtrl', function($scope,$http,toastr, Popeye){
     
-    $scope.categoryList = [];
+   
+    /* $scope.categoryList = [];
     // $scope.categoryType;
     $scope.categoryName;
     $scope.newCategory = {category:'', type:''};
@@ -10,6 +11,8 @@ app.controller('recurringExpensesCtrl', function($scope,$http,toastr){
         console.log(response.data);
     })
     $scope.submit = true;
+
+   
 
     $scope.submit = function() {
         $scope.newCategory.category = $scope.categoryName;
@@ -24,43 +27,43 @@ app.controller('recurringExpensesCtrl', function($scope,$http,toastr){
           });
        
          
-    }
+    } */
 });
-app.filter('unique', function () {
+// app.filter('unique', function () {
 
-    return function (items, filterOn) {
+//     return function (items, filterOn) {
 
-        if (filterOn === false) {
-            return items;
-        }
+//         if (filterOn === false) {
+//             return items;
+//         }
 
-        if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
-            var hashCheck = {}, newItems = [];
+//         if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+//             var hashCheck = {}, newItems = [];
 
-            var extractValueToCompare = function (item) {
-                if (angular.isObject(item) && angular.isString(filterOn)) {
-                    return item[filterOn];
-                } else {
-                    return item;
-                }
-            };
+//             var extractValueToCompare = function (item) {
+//                 if (angular.isObject(item) && angular.isString(filterOn)) {
+//                     return item[filterOn];
+//                 } else {
+//                     return item;
+//                 }
+//             };
 
-            angular.forEach(items, function (item) {
-                var valueToCheck, isDuplicate = false;
+//             angular.forEach(items, function (item) {
+//                 var valueToCheck, isDuplicate = false;
 
-                for (var i = 0; i < newItems.length; i++) {
-                    if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
-                        isDuplicate = true;
-                        break;
-                    }
-                }
-                if (!isDuplicate) {
-                    newItems.push(item);
-                }
+//                 for (var i = 0; i < newItems.length; i++) {
+//                     if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+//                         isDuplicate = true;
+//                         break;
+//                     }
+//                 }
+//                 if (!isDuplicate) {
+//                     newItems.push(item);
+//                 }
 
-            });
-            items = newItems;
-        }
-        return items;
-    };
-});
+//             });
+//             items = newItems;
+//         }
+//         return items;
+//     };
+// });
