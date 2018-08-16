b288c37bbcc057d2aeee3d44883609878f7ed471
Berina Dervisic <dervisicberina@hotmail.com>
2018-08-16 19:20:41 +0200

    cleanup code

--- material/controllers/loginCtrl.js
+++ material/controllers/loginCtrl.js
@@ -6,20 +6,11 @@ function loginCtrl($scope, $http, toastr, $location) {
         }
         return false;
     }
-
-    $scope.check_admin = function () {
-        if (localStorage.getItem('type') == "admin") {
-            return true;
-        }
-        return false;
-    }
-
     $scope.login = function (credentials) {
         $http.post('/login', credentials).then(function (response) {
             if (typeof response.data.token != 'undefined') {
                 $location.url('/charts');
                 localStorage.setItem('user', response.data.token);
-                localStorage.setItem('type', response.data.type);
                 toastr.success('You are successfully logged in!', 'Login Success!');
                 $location.url('/charts');
 
