ce2d59801b09da311b936a2bbb72fc888b07ce75
Berina Dervisic <dervisicberina@hotmail.com>
2018-08-25 21:50:57 +0200

    1

--- material/controllers/editUser.js
+++ material/controllers/editUser.js
@@ -5,12 +5,12 @@ function editUser($scope, $http,AuthenticationService){
     $scope.editUser = function (id) {
         var headers = { headers: { 'token': AuthenticationService.getToken() } }
         $http.get('/users/' + id, headers).then(function (response) {
-            $scope.users = response.data;
+            $scope.user = response.data;
         });
     };
     $scope.update = function () {
         var headers = { headers: { 'token': AuthenticationService.getToken() } }
-        $http.put('/users/' + $scope.users._id, $scope.user, headers).then(function (response) {
+        $http.put('/users/' + $scope.user._id, $scope.user, headers).then(function (response) {
             $scope.refresh();
             toastr.info("users updated!");
         });
