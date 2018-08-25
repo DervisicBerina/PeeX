427c503ca01135b57da75f3c86a994d758ffd4de
Berina Dervisic <dervisicberina@hotmail.com>
2018-08-25 16:14:20 +0200

    s

--- index.js
+++ index.js
@@ -205,7 +205,7 @@ app.put('/category/:id', function (req, res) {
     },
     update: {
       $set: {
-        name: req.body.category,
+        category: req.body.category,
       }
     },
     new: true
