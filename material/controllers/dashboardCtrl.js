f4ef9863b758d25d84673c6b1dc4de1fbfe6c287
Berina Dervisic <dervisicberina@hotmail.com>
2018-08-25 17:50:13 +0200

    s

--- material/controllers/dashboardCtrl.js
+++ material/controllers/dashboardCtrl.js
@@ -1,12 +1,4 @@
 function dashboardCtrl($scope, $http){
-    console.log("Initializing dashboard");
-    //     <script>
-    //     $(document).ready(function() {
-    //         // Javascript method's body can be found in assets/js/demos.js
-    //         md.initDashboardPageCharts();
-    //     });
-    // </script>
-        md.initDashboardPageCharts();
 
         $scope.loadExpenses = function () {
             var headers = { headers: { 'token': AuthenticationService.getToken() } }
