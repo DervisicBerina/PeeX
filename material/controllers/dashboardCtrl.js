4dbd59562d1383a03595fb9b74b726e915a6585c
Berina Dervisic <dervisicberina@hotmail.com>
2018-08-25 18:52:50 +0200

    d

--- material/controllers/dashboardCtrl.js
+++ material/controllers/dashboardCtrl.js
@@ -7,12 +7,12 @@ function dashboardCtrl($scope, $http, AuthenticationService){
 //     });
 // </script>
     md.initDashboardPageCharts();
-    // AuthenticationService.guardCustomerAuthenticated();
-    //     $scope.loadExpenses = function () {
-    //         var headers = { headers: { 'token': AuthenticationService.getToken() } }
-    //         $http.get('/expensesLastList', headers).then(function (response) {
-    //             $scope.myExpenses = response.data;
-    //         });
-    //     }
+    AuthenticationService.guardCustomerAuthenticated();
+        $scope.loadExpenses = function () {
+            var headers = { headers: { 'token': AuthenticationService.getToken() } }
+            $http.get('/expensesLastList', headers).then(function (response) {
+                $scope.myExpenses = response.data;
+            });
+        }
 
 }
