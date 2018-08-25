2b8ad4d59f16b9b93b526b0f5a34900bcc7d6a43
Berina Dervisic <dervisicberina@hotmail.com>
2018-08-25 13:37:56 +0200

    ,

--- material/controllers/categoryCtrl.js
+++ material/controllers/categoryCtrl.js
@@ -1,19 +1,6 @@
 function categoryCtrl($scope, $http, toastr, AuthenticationService) {
-
     AuthenticationService.guardCustomerAuthenticated();
     $scope.categoryList = [];
-    $scope.addButtonVisible = false;
-   
-    $scope.selectedCategory;
-    $scope.expense;
-    $scope.addButtonVisible = false;
-
-    $scope.hideAddButton = function () {
-        $scope.addButtonVisible = false;
-    }
-    $scope.showAddButton = function () {
-        $scope.addButtonVisible = true;
-    }
     var refresh = function () {
         var headers = { headers: { 'token': AuthenticationService.getToken() } };
         $http.get('/category', headers).then(function (response) {
@@ -25,16 +12,16 @@ function categoryCtrl($scope, $http, toastr, AuthenticationService) {
         $scope.visible = false;
         $scope.visible = $scope.visible = true;
     }
+
     $scope.close = function () {
         $scope.visible = true;
         $scope.visible = $scope.visible = false;
     }
-   
 
     $scope.categoryName;
     $scope.newCategory = { category: '' };
-    $scope.add = true;
-    $scope.add = function () {
+    $scope.submit = true;
+    $scope.submit = function () {
         var headers = { headers: { 'token': AuthenticationService.getToken() } }
         $scope.newCategory.category = $scope.categoryName;
         $http.post('/category', $scope.newCategory, headers).then(function successCallback(response) {
@@ -46,27 +33,4 @@ function categoryCtrl($scope, $http, toastr, AuthenticationService) {
         refresh();
 
     }
-    $scope.deleteCategory = function (id) {
-        var headers = { headers: { 'token': AuthenticationService.getToken() } }
-        $http.delete('/category/' + id, headers).then(function (response) {
-            $scope.refresh();
-            toastr.error('Deleted expense');
-        }
-        );
-    };
-
-    $scope.editCategory = function (id) {
-        var headers = { headers: { 'token': AuthenticationService.getToken() } }
-        $http.get('/category/' + id, headers).then(function (response) {
-            $scope.expense = response.data;
-        });
-    };
-    $scope.update = function () {
-        var headers = { headers: { 'token': AuthenticationService.getToken() } }
-        $http.put('/category/' + $scope.expense._id, $scope.expense, headers).then(function (response) {
-            $scope.refresh();
-            toastr.info("category updated!");
-        });
-    };
-    refresh();
 }
