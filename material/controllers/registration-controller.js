68bc5b17d542b2c659211529ba32972797ec12f0
Berina Dervisic <dervisicberina@hotmail.com>
2018-08-26 14:19:26 +0200

    s

--- material/controllers/registration-controller.js
+++ material/controllers/registration-controller.js
@@ -1,19 +1,15 @@
 function RegistrationController($scope, $http, toastr, $location) {
-  console.log("Hello from Registration Controller");
-
   $scope.add_user = function () {
     $http.post('/register', $scope.user).then(function (data) {
+      console.log(data.status);
       if (data.status == 202) {
-        toastr.error("Use another email");
-
+        toastr.info("Use another email");
       } else {
-
         // $scope.users_list = [];
         $scope.user = null;
         toastr.success("You are successfully registered! Please Login!", "Registration Successfull!");
         $location.url('/login');
         // $scope.users_list.push(data);
-
       }
     });
   }
